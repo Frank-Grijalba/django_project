35c4a062a84cf12e5c29624ccc6aeaa7a08a936f
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-25 06:59:37 -0500

    Personalizando el formulario del admin de django

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Registrado
+from core.forms import RegModelForm
+# Register your models here.
+
 
 class AdminRegistrado(admin.ModelAdmin):
     list_display = ["email", "nombre", "timestamp"]
+    form = RegModelForm
     # list_display_links = ["nombre"] # para corregir error de list_display: refers to the first field in 'list_display' ('email'), which cannot be used unless 'list_display_links' is set.
     list_filter = ["timestamp"]
     list_editable = ["nombre"]
     search_fields = ["email", "nombre"]
-    class Meta:
-        model = Registrado
+    # class Meta:
+    #     model = Registrado
 
 admin.site.register(Registrado, AdminRegistrado)
--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from blog.models import Registrado
 
-#formulario para ser añadido a una vista
+#formularios para ser añadido a una vista (views.py)
+
+class RegModelForm(forms.ModelForm):
+    class Meta:
+        model = Registrado
+        fields = ["nombre", "email"]
+
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
