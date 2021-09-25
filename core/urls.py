f05650f061f0e4bec4b5daf2cbe03462d20a6c1b
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-24 22:55:59 -0500

    Adcionando mi primera pagina index.html y configuracion de plantillas

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views as core_views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', core_views.functionHello)
+    path('hello/', core_views.functionHello),
+    url(r'', core_views.index, name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.views.generic import View
+from django.shortcuts import render
 
-#soporta todas las rutas que vamos a crear las
+#soporta todas las rutas que vamos a crear y que son llamadas en urls.py
 
+# Llamado con función
 def functionHello(request, *args, **kwargs):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Desde functionHello: Hello World!")
+
+# LLamada de una página web
+def index(request):
+    return render(request, "index.html", {})
