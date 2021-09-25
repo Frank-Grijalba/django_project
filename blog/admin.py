cdcdb2a5e4d71162ee4d5205d9d7ca1a016c5f98
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-24 21:39:35 -0500

    Creo modelo y aplicacion de mi programa

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Registrado
+
+class AdminRegistrado(admin.ModelAdmin):
+    list_display = ["email", "nombre", "timestamp"]
+    # list_display_links = ["nombre"] # para corregir error de list_display: refers to the first field in 'list_display' ('email'), which cannot be used unless 'list_display_links' is set.
+    list_filter = ["timestamp"]
+    list_editable = ["nombre"]
+    search_fields = ["email", "nombre"]
+    class Meta:
+        model = Registrado
+
+admin.site.register(Registrado, AdminRegistrado)
--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,13 @@
-from django.conf import settings
+from __future__ import unicode_literals
 from django.db import models
-from django.utils import timezone
 
-
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class Registrado(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now = False)
 
     def __str__(self):
-        return self.title
+        return self.email
+
+    def __unicode__(self):
+        return self.email
