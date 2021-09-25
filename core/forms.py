12a8c7bd338e0b790132f98b4ae21cfc66d5c909
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-25 06:03:23 -0500

    Agregando elementos a la bd a traves del formulario index.html

--- core/forms.py
+++ core/forms.py
@@ -3,4 +3,4 @@ from django import forms
 #formulario para ser añadido a una vista
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
-    edad = forms.IntegerField()
+    email = forms.EmailField()
