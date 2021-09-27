f6fbea780ffeb7b0618990dca3c2a23264a1bd23
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-26 19:19:15 -0500

    Añadiendo validaciones al formulario admin

--- core/forms.py
+++ core/forms.py
@@ -8,6 +8,15 @@ class RegModelForm(forms.ModelForm):
         model = Registrado
         fields = ["nombre", "email"]
 
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        # Validaciones a formulario admin
+        email_base, proveedor= email.split("@")
+        dominio, extension = proveedor.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Utiliza correo con .edu")
+        return email
+
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
